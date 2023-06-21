4891f9e1cd778b5868e3844a18f2b4f30b1d65a0
Daniel Gibim <DanielGibimdev@gmail.com>
2023-06-20 17:44:56 -0700

    Update Ex10.py

--- Lista 1/Ex10.py
+++ Lista 1/Ex10.py
@@ -1,4 +1,11 @@
 #10) Escreva um programa para ler um valor inteiro e verificar se este valor é par ou
 # impar.
 
+numero = int(input('Digite um número inteiro para saber se é par ou impar: '))
+restodivisao = numero % 2
 
+if restodivisao == 0:
+    print('O número digitado é par!')
+
+else:
+    print('O número digitado é impar!')
