f91d10c76e28da6a6396e51047c3853eec9c30d9
Daniel Gibim <DanielGibimdev@gmail.com>
2023-06-20 17:48:43 -0700

    Create Ex11.py

---
+++ Lista 1/Ex11.py
@@ -0,0 +1,10 @@
+# 11) Escrever um programa que determine se um número inteiro pode ser divisível por
+# três. Imprima uma mensagem.
+
+num = int(input('Digite um número inteiro: '))
+
+if num % 3 == 0:
+    print('O número digitado é divisível por 3.')
+
+else:
+    print('O número digitado não é divisível por 3.')
